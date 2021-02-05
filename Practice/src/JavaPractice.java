87d94903c480b74743b81bbb29d895af40e225f7
Armando Salazar <armando.salazar89@gmail.com>
2021-02-04 22:37:23 -0600

    "feat: sumOfTwoNumbers"

---
+++ Practice/src/JavaPractice.java
@@ -0,0 +1,8 @@
+public class JavaPractice {
+//    Create a method that takes two integers as arguments and return their sum.
+    public static int sumOfTwoNumbers(int a, int b){
+        return a + b;
+    }
+
+
+}
