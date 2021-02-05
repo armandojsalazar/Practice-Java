ba691f2d6b54ab04c2caad1f8dd71e902f2adf30
Armando Salazar <armando.salazar89@gmail.com>
2021-02-04 22:43:53 -0600

    "feat: public static int nextEdge(int side1, int side2)"

--- Practice/src/JavaPractice.java
+++ Practice/src/JavaPractice.java
@@ -12,5 +12,8 @@ public class JavaPractice {
             return false;
         }
     }
-
+// Create a function that finds the maximum range of a triangle's third edge, where the side lengths are all integers.
+public static int nextEdge(int side1, int side2) {
+   return (side1 + side2) - 1;
+}
 }
