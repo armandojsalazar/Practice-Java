6013927e66dc926ac74398fdf24753a6ba2dabfc
Armando Salazar <armando.salazar89@gmail.com>
2021-02-04 22:51:24 -0600

    "feat: public static boolean lessThanOrEqualToZero(int num)"

--- Practice/src/JavaPractice.java
+++ Practice/src/JavaPractice.java
@@ -21,4 +21,13 @@ public class JavaPractice {
 public static int power(int voltage, int current) {
     return voltage * current;
 }
+
+//Create a method that takes an integer as its only argument and returns true if it's less than or equal to zero, otherwise return false.
+public static boolean lessThanOrEqualToZero(int num) {
+if(num <= 0){
+    return true;
+}else{
+    return false;
+}
+}
 }
